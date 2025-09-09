d813a42904622becf7145c93f0a53186399cb859
prerana-e <Prerana.mg@gmail.com>
2025-09-08 19:52:25 -0700

    Step 1: Complete

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const axios = require('axios').default;
 
 async function getUsers() {
   try {
-    const response = await axios.get(`https://reqres.in/api/users`);
+    // const response = await axios.get(`https://reqres.in/api/users`);
+
+    const response = await axios.get('https://trends.google.com/home?geo=DO&hl=en-US');
+    console.log('Response received from Google Trends:');
+    console.log('Status Code:', response.status);
+    console.log('Headers:', response.headers);
+    console.log('Response Data:', response.data);
+    console.log('Full Response Object:');
+
     console.log(response);
   } catch (error) {
     console.error(error);
